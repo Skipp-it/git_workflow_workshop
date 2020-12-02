12873d90a2b2c595d5fd098c65629caf137fc58a
Celalalt666 <mihairaducroitoru@gmail.com>
2020-12-02 14:14:13 +0200

    Dictionary with available functions

---
+++ operations.py
@@ -0,0 +1,7 @@
+
+
+available_functions = {"+": calculate_addition,
+                       "-":calculate_subtraction,
+                       "/":calculate_division,
+                       "*": calculate_multiplication
+                       }
