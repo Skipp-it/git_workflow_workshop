5a49e01c54a085c87d36f3a1e0bf61d6a00e4415
mihaicaragea <61205164+mihaicaragea@users.noreply.github.com>
2020-12-02 14:19:08 +0200

    some ui  improvements

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 def ask_operation_type():
     print("Enter the operation type from the folowing options:")
     print("+ , - , * , / ")
-    return input()
+    return input("Enter your option:")
 
 
-    
